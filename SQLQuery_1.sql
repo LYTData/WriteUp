59ae45e790f3265c411342383da5d292c6f681f1
a-powers <awpowers@gmail.com>
2022-02-22 20:50:22 -0500

    update

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -1,5 +1,5 @@
-SELECT album.title, chinook.artist.name
+SELECT album.title as Title, chinook.artist.name as Artist
 FROM chinook.album
 JOIN chinook.artist using(artistid)
 WHERE name LIKE 'Led %' and title ILIKE '%disc%' and title ILIKE '%live%'
-ORDER BY title DESC;
+ORDER BY title;
